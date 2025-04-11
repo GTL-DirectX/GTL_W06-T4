38b793a1ff829a0c3cd870e7504a73ed59adb220
Joochan <joochan123123@gmail.com>
2025-04-11 19:52:01 +0900

    feat: Ubershader 기본변수 추가 및 VS, PS 함수 추가.

--- EngineSIU/EngineSIU/Shaders/UberLit.hlsl
+++ EngineSIU/EngineSIU/Shaders/UberLit.hlsl
@@ -2,42 +2,49 @@
 #define NUM_POINT_LIGHT 4
 #define NUM_SPOT_LIGHT 4
 
-struct FLightInfoBase
+struct FAmbientLightInfo
 {
     float3 Color;
     float Intensity;
 };
 
-struct FAmbientLightInfo : FLightInfoBase
+struct FDirectionalLightInfo
 {
-    float dummy;
-};
-
-struct FDirectionalLightInfo : FLightInfoBase
-{
+    float3 Color;
+    float Intensity;
     float3 Direction;
+    float Pad;
 };
 
-struct FPointLightInfo : FLightInfoBase
+struct FPointLightInfo
 {
+    float3 Color;
+    float Intensity;
     float3 Position;
     float AttenuationRadius;
     float LightFalloffExponent;
+    float2 Pad;
 };
 
-struct FSpotLightInfo : FPointLightInfo
+struct FSpotLightInfo
 {
+    float3 Color;
+    float Intensity;
+    float3 Position;
+    float AttenuationRadius;
+    float LightFalloffExponent;
     float3 Direction;
     float InnerConeAngle;
     float OuterConeAngle;
+    float2 Pad;
 };
 
 // 상위 쉐이더에서 모아서 통합 할 수 있을듯? 다른 객체들의 MVP와 함께.
 cbuffer PerObject : register(b0)
 {
-    row_major float4x4 World;
-    row_major float4x4 View;
-    row_major float4x4 Projection;
+    row_major matrix World;
+    row_major matrix View;
+    row_major matrix Projection;
 };
 
 cbuffer Lighting : register(b1)
@@ -66,3 +73,35 @@ struct VS_OUT
     float4 Position : SV_POSITION;
     float3 WorldNormal : NORMAL;
 };
+
+VS_OUT Uber_VS(VS_IN Input)
+{
+    VS_OUT output;
+#if LIGHTING_MODEL_GOURAUD
+
+#elif LIGHTING_MODEL_LAMBERT
+
+#elif LIGHTING_MODEL_PHONG
+
+#endif
+    
+    return output;
+}
+
+float4 Uber_PS(VS_OUT Input) : SV_TARGET
+{
+    //float4 finalPixel = TextureColor;
+    //finalPixel += Emissive;
+#if LIGHTING_MODEL_GOURAUD
+#elif LIGHTING_MODEL_LAMBERT
+    finalPixel += CalculateAmbientLight(...);
+    for(It : PointLights)
+    {
+        finalPixel += CalculatePointLight(...);
+    }
+#elif LIGHTING_MODEL_PHONG
+    // Specular Reflectance
+#endif
+    return float4(0.33, 0.33, 0.33, 0.33);
+    //return finalPixel;
+}
