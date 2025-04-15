7cb80f87366f054d5b8c41a3468a3f6cdf0f48b1
정재현 <ray1203@naver.com>
2025-04-15 21:25:43 +0900

    Lambert Light 수정
    1. 자동차의 창문만 빛나는 문제
    창문 diffuse가 0이라 발생. albedo 같이 사용
    2. Directional 반대로 적용
    Info 넣어줄 때 방향에 - 곱하고 쉐이더에서 - 다시 곱해서 발생.
    Info 넣어줄 때 방향 그대로 넣어줌
    3. spot light 방향 문제
    inner, outer이 반대로 적용되고 있었음.

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Light/DirectionalLightComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Light/DirectionalLightComponent.cpp
@@ -5,7 +5,7 @@ void UDirectionalLightComponent::UploadLightInfo(void* OutInfo) const
     FDirectionalLightInfo* Info = reinterpret_cast<FDirectionalLightInfo*>(OutInfo);
     Info->Color = GetDiffuseColor().ToFVector();
     Info->Intensity = GetIntensity();
-    Info->Direction = -GetForwardVector();
+    Info->Direction = GetForwardVector();
 }
 
 UObject* UDirectionalLightComponent::Duplicate(UObject* InOuter)
